03af923735ecccff40fbd616e2f0f5919f80040d
qiang.sun <qiang.sun@husor.com>
2018-09-22 12:22:24 +0800

    路由入门

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,23 @@
 Route::get('/', function () {
     return "Hello, World!";
 });
+
+Route::get('user/{id?}', function ($id = 1) {
+    return "用户ID: " . $id;
+})->name('user.profile');
+
+Route::get('page/{id}', function ($id) {
+    return '页面ID: ' . $id;
+})->where('id', '[0-9]+');
+
+Route::get('page/{name}', function ($name) {
+    return '页面名称: ' . $name;
+})->where('username', '[A-Za-z]+');
+
+Route::get('page/{id}/{slug}', function ($id, $slug) {
+    return $id . ':' . $slug;
+})->where(['id' => '[0-9]+', 'slug' => '[A-Za-z]+']);
+
+Route::get('test', function () {
+    return route('user.profile', [100]);
+});
